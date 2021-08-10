68a1a389b8936b72f0334763d657e9f5f7bd90bc
phihung3101 <88748885+phihung3101@users.noreply.github.com>
2021-08-11 05:37:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Thư à!',
+    introDesc: `Khi xưa Thúy Kiều ở lầu Ngưng Bích. Vừa nói chuyện với em anh đã thích`,
+    btnIntro: 'ahaha',
+    title: 'Hinh như anh đã thích em r CRUSH ơi 😙',
+    desc: 'Anh thích em rồi. mốt hết dịch em có hẹn hò với a không ',
+    btnYes: 'em sẽ cho 1 cơ hội <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Em giống ly cà phê a vừa uống v. Quá ngon nên khiến anh phải mất ngủ<3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu nói tiếng <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'sáng nt anh',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
